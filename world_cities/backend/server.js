afb246c2f60ba192dbbeffa96b1b00870e79fc24
Milton <104756300+Kummirai@users.noreply.github.com>
2025-09-11 14:14:14 +0200

    Create the server js file boiler plate

---
+++ world_cities/backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+
+const app = express();
+app.use(cors(corsOptions));
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
