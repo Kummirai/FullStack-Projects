bf37ef19e75fffacc056e64aa81b36b31190d107
Milton <104756300+Kummirai@users.noreply.github.com>
2025-09-11 13:02:31 +0200

    Create Header Component

--- world_cities/frontend/src/App.jsx
+++ world_cities/frontend/src/App.jsx
@@ -1,10 +1,13 @@
-function App() {
+import Header from "./components/Header";
+import InputField from "./components/InputField";
 
+function App() {
   return (
-    <>
-     
-    </>
-  )
+    <div className="container p-10">
+      <Header />
+      <InputField />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ world_cities/frontend/src/components/InputField.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InputField = () => {
+  return (
+    <div className="">
+      <div>
+        <form action="">
+          <input type="text" name="" id="" />
+          <button>Answer</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default InputField;
