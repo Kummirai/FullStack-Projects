b007f1142ca6ebaa71ad71eab813c55a7a73d5bd
Milton <104756300+Kummirai@users.noreply.github.com>
2025-09-11 11:37:26 +0200

    Use tailwind instead of boostrap in the project

--- library_app/backend/routes/indexRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {indexController} from '../controllers/indexController.js'
-
-const route = express.Router();
-
- route.get('/', indexController)
-
-
-export default route;
